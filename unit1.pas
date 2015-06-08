b97012fc4a674f42ae1f8921256de89e7ed59f5e
hendrayana <hendrayans40@yahoo.co.id>
2015-06-08 13:52:03 +0700

    tugas1

---
+++ unit1.pas
@@ -0,0 +1,47 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  showmessage(Edit1.Text);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  showmessage('Selamat datang');
+end;
+
+end.
+
